4005236068f88d058ebe98525edb23ac0e861582
jerry1938 <jerry5585@yahoo.com>
2021-10-13 13:27:54 +0200

    Added all method calls in the correct case and added comments to the method.

--- StartApp.cs
+++ StartApp.cs
@@ -4,8 +4,36 @@ namespace PrimeNumbers
 {
     public static class StartApp
     {
+        /// <summary>
+        /// Keeps the program running until the user decides to exit. Runs the
+        /// correct method depending on what the user wants to do in the
+        /// program.
+        /// </summary>
         public static void Start(){
-            MenuHandler.Menu();
+            bool exitProgram = false;
+            int choice;
+
+            do
+            {
+                choice = MenuHandler.Menu();
+                switch (choice)
+                {
+                    case 1:
+                        PrimeNumber.AddPrime();
+                        break;
+                    case 2:
+                        PrimeNumber.GetNextPrime();
+                        break;
+                    case 3:
+                        PrimeNumber.PrintDataStructure();
+                        break;
+                    case 4:
+                        exitProgram = true;
+                        break;
+                    default:
+                        break;
+                }
+            } while (!exitProgram);
         }
     }
 }
